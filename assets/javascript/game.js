c17d518710e8b5b289eb2f7707d59c04f143f6d6
Prakash Acharya <pracharya2601@gmail.com>
2019-07-09 18:13:05 -0700

    index and assets file upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessesMade = [];
+var userGuess = document.onkeyup;
+var compGuess = letters[Math.floor(Math.random() * letters.length)];
+
+document.onkeyup = function (event) {
+  
+  userGuess = event.key;
+  guessesMade.push(userGuess);
+
+  if (compGuess == userGuess) {
+    wins++;
+    guessesLeft = 10;
+    guessesMade = [];
+  }
+  
+  if (compGuess != userGuess) {
+    guessesLeft--;
+    
+  }
+
+  if (guessesLeft == 0) {
+    losses++;
+    guessesLeft = 10;
+    guessesMade = [];
+  }
+
+  var html =
+    '<p><h1>The Psychic Game</h1></p>' +
+    "<p> Guess what letter I'm thinking of</p>" +
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses Left: " + guessesLeft + "</p>" +
+    "<p>Your Guesses so far: " + guessesMade + "</p>";
+    document.querySelector("div").innerHTML = html;
+}
